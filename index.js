f3e819d111af01255a96eed8a29822376663d869
ch3rn1k <git@ch3rn1k.me>
2020-04-08 14:50:01 +0300

    added default boiling feature

--- index.js
+++ index.js
@@ -14,41 +14,85 @@ class MiMultipurposeKettle {
     if (!config.ip) throw new Error('Your must provide IP address of the Multipurpose Kettle!');
     if (!config.token) throw new Error('Your must provide token of the Multipurpose Kettle!');
 
-    let device = new Service.Switch(options.name);
+    let info = new Service.AccessoryInformation();
+    let device = new Service.Switch(config.name);
 
     this.log = log;
     this.ip = config.ip;
     this.token = config.token;
+    this.services = [device, info];
+
+    info
+    .setCharacteristic(Characteristic.Manufacturer, 'Xiaomi')
+    .setCharacteristic(Characteristic.Model, 'Kettle')
+    .setCharacteristic(Characteristic.SerialNumber, 'Undefined')
 
     /** On */
     device.getCharacteristic(Characteristic.On)
-    .on('set', this.setOn.bind(this))
+    .on('get', this.getStatus.bind(this))
+    .on('set', this.setWork.bind(this));
+
+    /** Looking for accessory. */
+    this.discover();
   }
 
-  getServices() {
-    return this.services;
+  async getStatus(callback) {
+    try {
+      const [result] = await this.device.call('get_prop', ['work_status']);
+
+      /**
+       *    1: Reservation
+       *    2: Cooking
+       *    3: Paused
+       *    4: Keeping
+       *    5: Stop
+      **/
+
+      callback(null, result === 2 || result === 3 || result === 4 ? true : false);
+    } catch (error) {
+      this.log.error('getStatus', error);
+      callback(error);
+    }
   }
 
   async discover() {
     try {
       this.device = await miio.device({ address: this.ip, token: this.token });
     } catch (error) {
-      this.log.error('Failed to discover the device. Next try in 1 minute!', error);
+      this.log.error('Failed to discover the device. Next try in 1 min!', error);
       setTimeout(() => { this.discover(); }, 60 * 1000);
     }
   }
 
-  async setOn(state, callback) {
+  async setWork(state, callback) {
     try {
-      const [result] = await this.device.call('set_work', [2,18,80,6,0]);
+      const [result] = await this.device.call('set_work', state ? [2, 18, 60, 0, 0] : [0, 18, 0, 0, 0]);
+
+      if (result !== 'ok')
+      throw new Error(result);
+
+      callback();
+    } catch (error) {
+      this.log.error('setWork', error);
+      callback(error);
+    }
+  }
+
+  async setVoice() {
+    try {
+      const [result] = await this.device.call('set_voice', [1]);
 
       if (result !== 'ok')
       throw new Error(result);
 
       callback();
     } catch (e) {
-      this.log.error('setOn', e);
+      this.log.error('setVoice', e);
       callback(e);
     }
+  }
+
+  getServices() {
+    return this.services;
   }
 }
